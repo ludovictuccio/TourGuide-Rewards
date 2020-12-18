5fc64eaad96e6ef114b52a4618819cb846df2fae
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2020-12-18 14:56:20 +0100

    Swagger 3

--- src/main/java/com/tourGuide/rewards/controllers/RewardsController.java
+++ src/main/java/com/tourGuide/rewards/controllers/RewardsController.java
@@ -15,6 +15,8 @@ import com.tourGuide.rewards.domain.UserReward;
 import com.tourGuide.rewards.domain.dto.UserRewardsDto;
 import com.tourGuide.rewards.services.IRewardsService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/rewards")
 public class RewardsController {
@@ -22,6 +24,7 @@ public class RewardsController {
     @Autowired
     public IRewardsService rewardsService;
 
+    @ApiOperation(value = "GET Attraction rewards", notes = "Need pathvariable attractionId & pathvariable userId - Return 200 OK or 404 Not Found -  Method used to return the integer of attraction rewards for an user.", response = Integer.class)
     @GetMapping("/getAttractionRewards/{attractionId}/{userId}")
     public int getAttractionRewards(
             @PathVariable("attractionId") final UUID attractionId,
@@ -29,6 +32,7 @@ public class RewardsController {
         return rewardsService.getAttractionRewards(attractionId, userId);
     }
 
+    @ApiOperation(value = "POST Calculate rewards", notes = "Need body UserRewardsDto - Return 200 OK - Method used to update user's rewards points, adding rewards if attraction was visited.", response = UserReward.class)
     @PostMapping("/calculateRewards")
     public List<UserReward> calculateRewards(
             @RequestBody final UserRewardsDto user) {
